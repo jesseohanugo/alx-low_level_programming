1bf40ca4a2df93fd05982748e9f33362830023f9
Jesse Ohanugo <jesseohanugo@gmail.com>
2022-12-13 13:03:13 -0800

    add 2-print_alphabet_x10.c; fix main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all alphabets 10 times
+ *
+ * Return - always void
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+	int i;
+	
+	i = 0;
+
+	while(i < 10)
+	{
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			++alphabet;
+		}
+
+		_putchar('\n');
+		++i;
+	}
+}
