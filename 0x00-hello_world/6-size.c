8f0e289464720716802b67554081f8a26d8792db
Jesse Ohanugo <jesseohanugo@gmail.com>
2022-12-13 09:48:33 -0800

    add some tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+int _putchar(char);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char* strPtr;
+	strPtr = "_putchar";
+
+	while(*strPtr != '\0') {
+		_putchar(*strPtr);
+		++strPtr;
+	}
+
+	_putchar('\n');
+	return(0);
+}
