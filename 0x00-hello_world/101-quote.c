3acc6a5ffac240e874f9090acb5e7de12be60b6f
Jesse Ohanugo <jesseohanugo@gmail.com>
2022-12-14 05:02:20 -0800

    add task 4 - 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, a, sizeof(a));
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,17 @@
  */
 int main(void)
 {
-	puts("Size of a char: 1 byte(s)");
-	puts("Size of an int: 4 byte(s)");
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+
+	return (0);
 }
