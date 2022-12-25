624282256376c44d91234fefb7ce5df2388359de
Jesse Ohanugo <jesseohanugo@gmail.com>
2022-12-25 16:23:54 +0100

    add task 12 of 0x06 project

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer to buffer
+ * @size: size in bytes of the buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10 && i + j < size; ++j)
+		{
+			printf("%02x", (b[i + j]));
+			(j + 1) % 2 == 0 ? putchar(' ') : 1;
+		}
+
+		for (; j < 10; ++j)
+			printf("%s", (j + 1) % 2 == 0 ? "   " : "  ");
+
+		for (j = 0; j < 10 && i + j < size; ++j)
+			printf("%c", isprint(b[i + j]) ? b[i + j] : '.');
+
+		putchar('\n');
+	}
+}
