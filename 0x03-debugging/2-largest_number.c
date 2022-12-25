5b2c07a25db59c80359519a745ce0dd316646218
Jesse Ohanugo <jesseohanugo@gmail.com>
2022-12-25 17:51:36 +0100

    fix add README.md and betty coding style

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,14 +10,14 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
 	largest = a;
-    if (b > largest)
-        largest = b;
+	if (b > largest)
+		largest = b;
 
 	if (c > largest)
 		largest = c;
 
-    return (largest);
+	return (largest);
 }
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Solution for alx 0x03. C - Debugging project
