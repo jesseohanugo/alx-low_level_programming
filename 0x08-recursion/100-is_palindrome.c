4be4f4294e2592fd1889e5ffa979f39b8049978c
Jesse Ohanugo <jesseohanugo@gmail.com>
2023-01-05 16:56:28 +0100

    fix 100-is_palindrome.c: betty coding style

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,7 +16,13 @@ int is_palindrome(char *s)
 	return (check_palindrome(s, index));
 }
 
-
+/**
+ * check_palindrome - finds palindrome
+ * @pd: palindrome string
+ * @index: end of string
+ *
+ * Return: 1 or 0
+ */
 int check_palindrome(char *pd, int index)
 {
 	if (*pd == '\0')
@@ -28,10 +34,16 @@ int check_palindrome(char *pd, int index)
 	return (check_palindrome(pd + 1, index - 2));
 }
 
+/**
+ * get_index - returns index of last element
+ * @s: string
+ *
+ * Return: index of last element
+ */
 int get_index(char *s)
 {
 	if (*s == '\0')
 		return (-1);
 
-	return get_index(s + 1) + 1;
+	return (get_index(s + 1) + 1);
 }
